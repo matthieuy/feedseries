77427ee8c4b5ed213e208b3df37ea164705e66bd
Matthieu YK <matthieuy@riseup.net>
2018-02-13 11:56:17 +0100

    About version

--- src/renderer/components/modal/About.vue
+++ src/renderer/components/modal/About.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     About
+    {{ platform }}
   </div>
 </template>
 
 <script>
   export default {
+    data () {
+      return {
+        platform: require('os').platform(),
+      }
+    },
   }
 </script>
