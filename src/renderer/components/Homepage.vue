13ebeee9274fe7c680999a3248b7383847723311
Matthieu <matthieuy@riseup.net>
2018-02-14 21:27:59 +0100

    Homepage : design

--- src/renderer/components/Homepage.vue
+++ src/renderer/components/Homepage.vue
@@ -1,68 +1,48 @@
 <template>
     <div class="profil">
-      <div class="infos" v-show="stats.id">
-        <img :src="stats.avatar" alt="" class="avatar">
-
+      <div v-show="stats.id">
         <div class="fleft">
-          <div class="binfo" v-if="stats.friends">
-            <i class="fa fa-users"></i>
-            {{ stats.friends|plurialize('ami', 'amis') }}
-          </div>
-
-          <div class="binfo">
-            <i class="fa fa-certificate"></i>
-            {{ stats.xp }} XP
-          </div>
-
-          <div class="binfo" v-if="stats.comments">
-            <i class="fa fa-comments"></i>
-            {{ stats.comments|plurialize('commentaire', 'commentaires') }}
-          </div>
+          <img :src="stats.avatar" alt="" class="avatar">
         </div>
-
-        <div class="fleft">
-          <div class="binfo">
-            <div><i class="fa fa-clock"></i> {{ stats.time_on_tv|duration_tv }}</div>
-            <div class="pull-right" v-if="stats.episodes">soit {{ stats.episodes|plurialize('épisode', 'épisodes') }} vus</div>
+        <div class="fleft cols">
+          <div class="fleft col20">
+            <div class="binfo" v-if="stats.friends"><i class="fa fa-users"></i> {{ stats.friends|plurialize('ami', 'amis') }}</div>
+            <div class="binfo"><i class="fa fa-certificate"></i> {{ stats.xp }} XP</div>
+            <div class="binfo" v-if="stats.comments"><i class="fa fa-comments"></i> {{ stats.comments|plurialize('commentaire', 'commentaires') }}</div>
+            <div class="binfo" v-if="stats.badges"><i class="fa fa-bookmark"></i> {{ stats.badges|plurialize('badge', 'badges') }}</div>
+            <div class="binfo" v-if="stats.written_words"><i class="fa fa-pencil-alt"></i> {{ stats.written_words|plurialize('mot écrit', 'mots écrits') }}</div>
             <div class="clearfix"></div>
           </div>
-
-          <div class="binfo" v-if="stats.time_to_spend">
-            <i class="fa fa-hourglass"></i>
-            Encore {{ stats.time_to_spend|duration_tv }} - {{ stats.episodes_to_watch|plurialize('épisode', 'épisodes') }}
+          <div class="fleft col60">
+            <div class="binfo">
+              <div id="showChart"></div>
+              <div class="clearfix"></div>
+            </div>
           </div>
-
-          <div class="binfo">
-            <i class="fa fa-star"></i>
-            "{{ stats.favorite_genre }}" à mater le {{ stats.favorite_day }}
-          </div>
-        </div>
-
-        <div class="fleft">
-          <div class="binfo">
-            <i class="fa fa-chart-line"></i>
-            {{ stats.episodes_per_month|plurialize('épisode', 'épisodes') }} / mois
-          </div>
-
-          <div class="binfo">
-              <i class="fa fa-calendar-check"></i>
-              {{ stats.streak_days|plurialize('jour consécutif', 'jours consécutifs') }}
-          </div>
-        </div>
-
-        <div class="fleft">
-          <div class="binfo">
-            <div id="showChart"></div>
-            <div class="clearfix"></div>
+          <div class="fleft col20">
+            <div class="binfo"><i class="fa fa-chart-line"></i> {{ stats.episodes_per_month|plurialize('épisode', 'épisodes') }} / mois</div>
+            <div class="binfo"><i class="fa fa-calendar-check"></i> {{ stats.streak_days|plurialize('jour consécutif', 'jours consécutifs') }}</div>
+            <div class="binfo"><i class="fa fa-birthday-cake"></i> Inscrit depuis {{ stats.member_since_days|plurialize('jour', 'jours') }}</div>
+            <div class="binfo"><i class="fa fa-star"></i> "{{ stats.favorite_genre }}" à mater le {{ stats.favorite_day }}</div>
           </div>
         </div>
         <div class="clearfix"></div>
+        <div class="linear-stats">
+          <div class="fleft binfo">
+            <i class="fa fa-clock"></i> {{ stats.time_on_tv|duration_tv }}
+            <span v-if="stats.episodes">({{ stats.episodes|plurialize('épisode vu', 'épisodes vus') }})</span>
+          </div>
+          <div class="fleft binfo" v-if="stats.time_to_spend">
+            <i class="fa fa-hourglass"></i>
+            Encore {{ stats.time_to_spend|duration_tv }} ({{ stats.episodes_to_watch|plurialize('épisode', 'épisodes') }})
+          </div>
+          <div class="clearfix"></div>
+        </div>
       </div>
-
       <div v-show="favorites.length">
         <h1 class="text-center">Favoris</h1>
         <div class="favorites">
-          <span  v-for="show in favorites">
+          <span v-for="show in favorites">
             <router-link :to="{ name: 'show', params: { id: show._id }}" class="img">
               <img :src="'https://www.betaseries.com/images/fonds/poster/' + show.poster" width="200" :alt="show.title">
             </router-link>
@@ -111,13 +91,14 @@
             {
               type: 'doughnut',
               startAngle: 180,
+              indexLabelFontSize: 14,
               indexLabel: '{y} {label}',
               toolTipContent: '{y} séries {label} : #percent%',
               dataPoints: [
-                { label: 'abandonnées', y: stats.shows_abandoned },
-                { label: 'en cours', y: stats.shows_current },
-                { label: 'à voir', y: stats.shows_to_watch },
-                { label: 'terminées', y: stats.shows_finished },
+                { label: 'abandonnées', y: stats.shows_abandoned, color: '#fdbc40' },
+                { label: 'en cours', y: stats.shows_current, color: '#34c84a' },
+                { label: 'à voir', y: stats.shows_to_watch, color: '#004012' },
+                { label: 'terminées', y: stats.shows_finished, color: '#fc605b' },
               ],
             },
           ],
@@ -142,39 +123,54 @@
 
 <style lang="scss">
   .profil {
-    .infos {
-      margin: 10px;
-      .avatar {
-        width: 130px;
-        height: 130px;
-        border-radius: 65px;
-        float: left;
-        margin-right: 20px;
-        box-shadow: 7px 6px 10px #181A1F;
+    .avatar {
+      width: 130px;
+      height: 130px;
+      border-radius: 65px;
+      margin: 20px 10px 0 10px;
+      box-shadow: 7px 6px 10px #181A1F;
+    }
+    .cols {
+      width: calc(100% - 160px);
+    }
+    .col20 {
+      width: 20%;
+    }
+    .col60 {
+      width: 60%;
+    }
+    .binfo {
+      float: none;
+      overflow: hidden;
+    }
+    #showChart {
+      min-height: 200px;
+      .canvasjs-chart-container {
+        width: 400px;
+        margin: auto;
       }
+    }
+    .linear-stats {
       .binfo {
-        float: none;
+        float: left;
+        width: calc(50% - 10px);
       }
     }
     .favorites {
-        max-height: 320px;
-        display: block;
-        overflow-y: hidden;
-        overflow-x: auto;
-        white-space: nowrap;
-        padding-bottom: 10px;
-        img {
-            display: inline-block;
-            margin: 5px;
-            box-shadow: 7px 6px 12px #181A1F;
-        }
-        &::-webkit-scrollbar {
-            height: 12px;
-        }
+      max-height: 320px;
+      display: block;
+      overflow-y: hidden;
+      overflow-x: auto;
+      white-space: nowrap;
+      padding-bottom: 10px;
+      img {
+        display: inline-block;
+        margin: 5px;
+        box-shadow: 7px 6px 12px #181A1F;
+      }
+      &::-webkit-scrollbar {
+        height: 12px;
+      }
     }
-  }
-  #showChart {
-    width: 400px;
-    height: 200px;
   }
 </style>
